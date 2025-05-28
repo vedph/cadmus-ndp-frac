35006c7ba148be7be7928c2794e374e3bd2c0d28
Myrmex <dfusi@hotmail.com>
2025-05-28 15:24:58 +0200

    More tests

---
+++ Cadmus.NdpFrac.Parts.Test/CodFrRulingsPartTest.cs
@@ -0,0 +1,103 @@
+﻿using Cadmus.Core;
+using Cadmus.NdpFrac.Parts.Test;
+using Cadmus.Seed.NdpFrac.Parts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cadmus.NdpFrac.Parts.Test;
+
+public sealed class CodFrRulingsPartTest
+{
+    private static CodFrRulingsPart GetPart()
+    {
+        CodFrRulingsPartSeeder seeder = new();
+        IItem item = new Item
+        {
+            FacetId = "default",
+            CreatorId = "zeus",
+            UserId = "zeus",
+            Description = "Test item",
+            Title = "Test Item",
+            SortKey = ""
+        };
+        return (CodFrRulingsPart)seeder.GetPart(item, null, null)!;
+    }
+
+    private static CodFrRulingsPart GetEmptyPart()
+    {
+        return new CodFrRulingsPart
+        {
+            ItemId = Guid.NewGuid().ToString(),
+            RoleId = "some-role",
+            CreatorId = "zeus",
+            UserId = "another",
+        };
+    }
+
+    [Fact]
+    public void Part_Is_Serializable()
+    {
+        CodFrRulingsPart part = GetPart();
+
+        string json = TestHelper.SerializePart(part);
+        CodFrRulingsPart part2 =
+            TestHelper.DeserializePart<CodFrRulingsPart>(json)!;
+
+        Assert.Equal(part.Id, part2.Id);
+        Assert.Equal(part.TypeId, part2.TypeId);
+        Assert.Equal(part.ItemId, part2.ItemId);
+        Assert.Equal(part.RoleId, part2.RoleId);
+        Assert.Equal(part.CreatorId, part2.CreatorId);
+        Assert.Equal(part.UserId, part2.UserId);
+
+        Assert.Equal(part.Rulings.Count, part2.Rulings.Count);
+    }
+
+    [Fact]
+    public void GetDataPins_NoEntries_Ok()
+    {
+        CodFrRulingsPart part = GetPart();
+        part.Rulings.Clear();
+
+        List<DataPin> pins = [.. part.GetDataPins(null)];
+
+        Assert.Single(pins);
+        DataPin pin = pins[0];
+        Assert.Equal("tot-count", pin.Name);
+        TestHelper.AssertPinIds(part, pin);
+        Assert.Equal("0", pin.Value);
+    }
+
+    [Fact]
+    public void GetDataPins_Entries_Ok()
+    {
+        CodFrRulingsPart part = GetEmptyPart();
+
+        for (int n = 1; n <= 3; n++)
+        {
+            part.Rulings.Add(new CodFrRuling
+            {
+                System = n % 2 == 0 ? "latn" : "grek",
+                Type = n % 2 == 0 ? "type1" : "type2",
+                Features = n % 2 == 0 ? ["dry"] : ["lead"]
+            });
+        }
+
+        List<DataPin> pins = [.. part.GetDataPins(null)];
+
+        Assert.Equal(5, pins.Count);
+
+        DataPin? pin = pins.Find(p => p.Name == "tot-count");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        Assert.Equal("3", pin!.Value);
+
+        // TODO: assert counts and values e.g.:
+        // pin = pins.Find(p => p.Name == "pos-bottom-count");
+        // Assert.NotNull(pin);
+        // TestHelper.AssertPinIds(part, pin!);
+        // Assert.Equal("2", pin.Value);
+    }
+}
---
+++ Cadmus.Seed.NdpFrac.Parts/CodFrRulingsPartSeeder.cs
@@ -0,0 +1,57 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.NdpFrac.Parts;
+using Fusi.Tools.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Seed.NdpFrac.Parts;
+
+/// <summary>
+/// Seeder for <see cref="CodFrRulingsPart"/>.
+/// Tag: <c>seed.it.vedph.ndp.cod-fr-rulings</c>.
+/// </summary>
+/// <seealso cref="PartSeederBase" />
+[Tag("seed.it.vedph.ndp.cod-fr-rulings")]
+public sealed class CodFrRulingsPartSeeder : PartSeederBase
+{
+    private static List<CodFrRuling> GetCodFrRulings(int count)
+    {
+        List<CodFrRuling> rulings = [];
+
+        for (int i = 0; i < count; i++)
+        {
+            CodFrRuling ruling = new Faker<CodFrRuling>()
+                .RuleFor(r => r.System, f => f.PickRandom("latn", "grek"))
+                // TODO use thesauri for type and features
+                .RuleFor(r => r.Type, f => f.PickRandom("type1", "type2"))
+                .RuleFor(r => r.Features, f => [f.PickRandom("dry", "lead")])
+                .Generate();
+            rulings.Add(ruling);
+        }
+
+        return rulings;
+    }
+
+    /// <summary>
+    /// Creates and seeds a new part.
+    /// </summary>
+    /// <param name="item">The item this part should belong to.</param>
+    /// <param name="roleId">The optional part role ID.</param>
+    /// <param name="factory">The part seeder factory. This is used
+    /// for layer parts, which need to seed a set of fragments.</param>
+    /// <returns>A new part or null.</returns>
+    /// <exception cref="ArgumentNullException">item or factory</exception>
+    public override IPart? GetPart(IItem item, string? roleId,
+        PartSeederFactory? factory)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        CodFrRulingsPart part = new Faker<CodFrRulingsPart>()
+           .RuleFor(p => p.Rulings, f => GetCodFrRulings(f.Random.Number(1, 3)))
+           .Generate();
+        SetPartMetadata(part, roleId, item);
+
+        return part;
+    }
+}
