04e811ea949ff75816f32e465e9272559a4c5c80
Myrmex <dfusi@hotmail.com>
2025-05-28 15:54:26 +0200

    More seeders

---
+++ Cadmus.NdpFrac.Parts.Test/CodFrSupportPartTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadmus.NdpFrac.Parts.Test;
+
+public sealed class CodFrSupportPartTest
+{
+}
